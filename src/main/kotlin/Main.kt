414233208ceab5bf5ba338b37758a769885bf9b5
oshai <ohadshai@gmail.com>
2018-09-14 12:34:22 +0300

    add javalin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,28 +2,36 @@ package com.github.jasync.mysql.example
 
 import com.github.jasync.sql.db.Configuration
 import com.github.jasync.sql.db.Connection
-import com.github.jasync.sql.db.general.ArrayRowData
 import com.github.jasync.sql.db.postgresql.PostgreSQLConnection
+import io.javalin.Javalin
+import io.javalin.JavalinEvent
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
-  logger.info("starting")
-
-  val connection: Connection = PostgreSQLConnection(
-      Configuration(
-          username = "username",
-          password = "password",
-          host = "host.com",
-          port = 5432,
-          database = "schema"
-      )
-  )
-  connection.connect().get()
-  val future = connection.sendPreparedStatement("select 0")
-  val queryResult = future.get()
-  println((queryResult.rows!![0] as ArrayRowData).columns.toList())
-  println((queryResult.rows!![1] as ArrayRowData).columns.toList())
-  connection.disconnect().get()
+    val connection: Connection = PostgreSQLConnection(
+            Configuration(
+                    username = "username",
+                    password = "password",
+                    host = "host.com",
+                    port = 5432,
+                    database = "schema"
+            )
+    )
+    val app = Javalin.create()
+            .event(JavalinEvent.SERVER_STARTING) {
+                logger.info("--- SERVER STARTING!")
+                connection.connect().get()
+                logger.info("--- connection STARTED!")
+            }
+            .event(JavalinEvent.SERVER_STOPPING) {
+                logger.info("--- SERVER STOPPING!")
+                connection.disconnect().get()
+                logger.info("--- connection STOPPED!")
+            }
+            .start(7000)
+    app.get("/") { ctx ->
+        ctx.result(connection.sendPreparedStatement("select 0"))
+    }
 }

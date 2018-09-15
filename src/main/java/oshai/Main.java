aa68f34cd30452a062ef3dcd230ee34724ab0d44
oshai <oshai@outbrain.com>
2018-09-15 22:18:29 +0300

    postgres java version

---
+++ src/main/java/oshai/Main.java
@@ -0,0 +1,42 @@
+package oshai;
+
+import com.github.jasync.sql.db.Configuration;
+import com.github.jasync.sql.db.Connection;
+import com.github.jasync.sql.db.postgresql.PostgreSQLConnection;
+import io.javalin.Javalin;
+import io.javalin.JavalinEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    Connection connection = new PostgreSQLConnection(
+      new Configuration(
+        "postgres",
+        "localhost",
+        5432,
+        "mysecretpassword",
+        "postgres"
+      )
+    );
+    Javalin app = Javalin.create()
+      .event(JavalinEvent.SERVER_STARTING, () -> {
+        logger.info("--- SERVER STARTING!");
+        connection.connect().get();
+        logger.info("--- connection STARTED!");
+      })
+      .event(JavalinEvent.SERVER_STOPPING, () -> {
+        logger.info("--- SERVER STOPPING!");
+        connection.disconnect().get();
+        logger.info("--- connection STOPPED!");
+      })
+      .start(7000);
+
+    app.get("/", (ctx) -> ctx.result(
+      connection.sendPreparedStatement("select 0")
+        .thenApply((t) -> "got result: " + t.getRows().get(0).get(0))
+    ));
+  }
+}

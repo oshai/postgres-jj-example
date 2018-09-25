23f161b027f402b41b4c2ca45a2ef9eed274ff8e
oshai <oshai@outbrain.com>
2018-09-25 16:54:06 +0300

    add connection pool

--- src/main/java/oshai/Main.java
+++ src/main/java/oshai/Main.java
@@ -3,27 +3,36 @@ package oshai;
 import com.github.jasync.sql.db.Configuration;
 import com.github.jasync.sql.db.Connection;
 import com.github.jasync.sql.db.QueryResult;
-import com.github.jasync.sql.db.postgresql.PostgreSQLConnection;
+import com.github.jasync.sql.db.pool.ConnectionPool;
+import com.github.jasync.sql.db.pool.PoolConfiguration;
+import com.github.jasync.sql.db.postgresql.pool.PostgreSQLConnectionFactory;
 import io.javalin.Javalin;
 import io.javalin.JavalinEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
-    Connection connection = new PostgreSQLConnection(
-      new Configuration(
+    PoolConfiguration poolConfiguration = new PoolConfiguration(
+      100,                            // maxObjects
+      TimeUnit.MINUTES.toMillis(15),  // maxIdle
+      10_000,                         // maxQueueSize
+      TimeUnit.SECONDS.toMillis(30)   // validationInterval
+    );
+    Connection connection = new ConnectionPool<>(
+      // for PostgreSQL use PostgreSQLConnectionFactory instead of MySQLConnectionFactory
+      new PostgreSQLConnectionFactory(new Configuration(
         "postgres",
         "localhost",
         5432,
         "mysecretpassword",
         "postgres"
-      )
-    );
+      )), poolConfiguration);
     Javalin app = Javalin.create()
       .event(JavalinEvent.SERVER_STARTING, () -> {
         logger.info("--- SERVER STARTING!");
